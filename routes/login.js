85d8cbf014c8edb04483d4ffbded9f48b9508820
djibril soukouna <djib_cisse_9@hotmail.com>
2023-07-13 15:42:18 +0200

    test

--- routes/login.js
+++ routes/login.js
@@ -1,47 +1,48 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-require('../models/connection');
-const User = require('../models/users');
-const { checkBody } = require('../modules/checkBody');
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
+// require('../models/connection');
+// const User = require('../models/users');
+// const { checkBody } = require('../modules/checkBody');
+// const uid2 = require('uid2');
+// const bcrypt = require('bcrypt');
 
-const token = uid2(32);
-const hash = bcrypt.hashSync(req.body.password, 10);
+// const token = uid2(32);
 
 
-// Sign up route :
-router.post('/signup', (res, req) => {
-
-  //check  is all fields are filled  
-  if (!checkBody(req.body, ['username', 'pseudo', 'password',])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
-    return;
-  }
+// // Sign up route :
+// router.post('/signup', (res, req) => {
+//   const hash = bcrypt.hashSync(req.body.password, 10);
   
-  User.findOne({ pseudo : req.body.pseudo })
-  .then(data => {
-    // register user if not existing
-    if (data === null) {
-      const newUser = new User ({
-        username: req.body.username,
-        pseudo: req.body.pseudo,
-        password: hash,
-        token: token,
-        isConnected: true,
-      })
+//   //check  is all fields are filled  
+//   if (!checkBody(req.body, ['username', 'pseudo', 'password',])) {
+//     res.json({ result: false, error: 'Missing or empty fields' });
+//     return;
+//   }
+  
+//   User.findOne({ pseudo : req.body.pseudo })
+//   .then(data => {
+//     // register user if not existing
+//     if (data === null) {
+//       const newUser = new User ({
+//         username: req.body.username,
+//         pseudo: req.body.pseudo,
+//         password: hash,
+//         token: token,
+//         isConnected: true,
+//       })
 
-      newUser.save()
-      .then(data => {
-        res.json({ result: true, token: data.token })
-      })
+//       newUser.save()
+//       .then(data => {
+//         res.json({ result: true, token: data.token })
+//       })
 
-    // if user already exists => error
-    } else {
-      res.json({ result: false, error: "user already exists" })
-    }
-  })
+//     // if user already exists => error
+//     } else {
+//       res.json({ result: false, error: "user already exists" })
+//     }
+//   })
 
-})
+// })
 
+// module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
+const token = uid2(32);
+
+
+// Sign up route :
+router.post('/signup', (res, req) => {
+  const hash = bcrypt.hashSync(req.body.password, 10);
+  
+  //check  is all fields are filled  
+  if (!checkBody(req.body, ['username', 'pseudo', 'password',])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+  
+  User.findOne({ pseudo : req.body.pseudo })
+  .then(data => {
+    // register user if not existing
+    if (data === null) {
+      const newUser = new User ({
+        username: req.body.username,
+        pseudo: req.body.pseudo,
+        password: hash,
+        token: token,
+        isConnected: true,
+      })
+
+      newUser.save()
+      .then(data => {
+        res.json({ result: true, token: data.token })
+      })
+
+    // if user already exists => error
+    } else {
+      res.json({ result: false, error: "user already exists" })
+    }
+  })
+
+})
 
 module.exports = router;
