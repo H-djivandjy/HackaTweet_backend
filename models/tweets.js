31c07d9a24bef951077455cd311200cb43530968
H-Djivandjy <houned.djivandjy@eemi.com>
2023-07-13 13:23:42 +0200

    adding Schema & Model

---
+++ models/tweets.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const hashtagsSchema = mongoose.Schema({
+    hashtagMsg: String,
+})
+
+const tweetsSchema = mongoose.Schema({
+    message: String,
+    date: Date,
+    hashtag: hashtagsSchema,
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
+
+})
+
+const Tweet = mongoose.model('tweets', tweetsSchema)
---
+++ models/users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+const usersSchema = mongoose.Schema({
+    username: String,
+    pseudo: String,
+    password: String,
+    token: String,
+    isConnected: Boolean,
+})
+
+const User = mongoose.model('users', usersSchema)
